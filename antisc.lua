5dfee01e4b054b9318eba226c809f1aaf4e62c00
nyepnyep <=>
2020-08-29 01:34:35 +0200

    Revert "Added base files"
    This reverts commit d864dac4aebc8d3fe5f6571a0480bfbd9f65a9b6.

--- antisc.lua
+++
@@ -1,9 +0,0 @@
-antisc = createMarker (0, 0, 0, "corona", 5, 85, 85, 85, 85) -- size 5, r: 85, g: 85, b: 85, a: 85
-
-if source == antisc then
-    local x, y, z = getElementRotation (vehicle)
-    if (x < 150) and (x > 270) then
-        blowVehicle (vehicle)
-        outputChatBox (x)
-    end
-end
