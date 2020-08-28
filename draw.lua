c3c0630e8613fa23c734e4544c493267dbc1c45f
nyepnyep <=>
2020-08-29 01:34:46 +0200

    Revert "Revert "Added base files""
    This reverts commit 5dfee01e4b054b9318eba226c809f1aaf4e62c00.

---
+++ draw.lua
@@ -0,0 +1,12 @@
+function changeDistance()
+    for i,object in pairs(getElementsByType("object")) do
+        if isElement(object) then
+            local elementID = getElementModel(object )
+            engineSetModelLODDistance(elementID,1000)
+        end
+    end
+end
+addEventHandler("onClientResourceStart",getResourceRootElement(getThisResource()),changeDistance)
+
+setFarClipDistance (1000)
+outputChatBox ("Updated")
