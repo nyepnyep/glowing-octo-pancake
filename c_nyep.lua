79b32d1cc7cdd5bb8ef36645ab3c3bf1c4a6aff2
nyepnyep <=>
2020-08-29 02:02:11 +0200

    Update: 29-9-2020 20:01AM
    Updated c_nyep.lua to include more marker triggers

--- c_nyep.lua
+++ c_nyep.lua
@@ -4,12 +4,28 @@ local gMe=getLocalPlayer()
 
 --[[ markers ]]
 marker1 = createMarker (0, 0, 0, "corona", 5, 85, 85, 85, 85) -- posX, posY, posZ, "type", size, r, g, b, a
+marker2 = createMarker (0, 0, 0, "corona", 5, 85, 85, 85, 85) -- posX, posY, posZ, "type", size, r, g, b, a
+marker3 = createMarker (0, 0, 0, "corona", 5, 85, 85, 85, 85) -- posX, posY, posZ, "type", size, r, g, b, a
+marker4 = createMarker (0, 0, 0, "corona", 5, 85, 85, 85, 85) -- posX, posY, posZ, "type", size, r, g, b, a
+marker5 = createMarker (0, 0, 0, "corona", 5, 85, 85, 85, 85) -- posX, posY, posZ, "type", size, r, g, b, a
 
 --[[ functions ]]
 function nyepnyep(hitPlayer)
     if hitPlayer~=gMe then return end
     vehicle=getPedOccupiedVehicle(hitPlayer)
     if source == marker1 then
+        -- add script here
+    end
+    if source == marker2 then
+        -- add script here
+    end
+    if source == marker3 then
+        -- add script here
+    end
+    if source == marker4 then
+        -- add script here
+    end
+    if source == marker5 then
         -- add script here
     end
 end
