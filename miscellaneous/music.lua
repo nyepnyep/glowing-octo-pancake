d3f957e8d149cc1b6ee4873041b89408891f3237
nyepnyep <36038368+nyepnyep@users.noreply.github.com>
2021-03-01 11:12:15 +0100

    Create music.lua
    simple playSound for playing music in a map

---
+++ miscellaneous/music.lua
@@ -0,0 +1,11 @@
+--[[
+    Replace "filename.mp3" with the name of your mp3 file
+
+    meta:
+
+    <file src="files/filename.mp3"></file>
+
+    NOTE: keep the mp3 file in the files/ directory, this makes for a more organized root directory
+]]
+
+playSound ("files/filename.mp3", true)
